b375a2b1bdccbc3eaa2f75d4b1a96a72bbf68694
lahinsaleem03@gmail.com <lahinsaleem03@localhost>
2024-05-21 18:40:05 +0530

    First commit

---
+++ Project0Test3/src/main/java/com/seproject/Project0Test3Application.java
@@ -0,0 +1,13 @@
+package com.seproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project0Test3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project0Test3Application.class, args);
+	}
+
+}
---
+++ Project0Test3/src/test/java/com/seproject/Project0Test3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project0Test3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
