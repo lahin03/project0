29f3846a690393df92f771443eec41f55eb1fe21
lahinsaleem03@gmail.com <lahinsaleem03@localhost>
2024-05-21 18:11:40 +0530

    First commit

---
+++ Project0Test2/src/main/java/com/seproject/Project0Test2Application.java
@@ -0,0 +1,13 @@
+package com.seproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project0Test2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project0Test2Application.class, args);
+	}
+
+}
